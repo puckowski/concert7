f82e0c220f561cf5eded2506f436692429022e7b
Daniel Puckowski <puckowski.d@gmail.com>
2024-07-08 17:30:32 -0400

    Update to Concert 9.0
    * Update to Concert 9.0.

--- Concert/Lang3/header.h
+++ Concert/Lang3/header.h
@@ -22,7 +22,7 @@ class ObjectStore;
 
 const std::wstring SOFTWARE_NAME = L"Concert";
 const std::wstring SOFTWARE_VERSION_STRING = L"0.0.9.0";
-const std::wstring SOFTWARE_VERSION_CODE = L"1380";
+const std::wstring SOFTWARE_VERSION_CODE = L"1385";
 const std::wstring SOFTWARE_AUTHOR = L"Daniel Puckowski";
 
 extern std::unordered_map<std::wstring, std::wstring> varLockMap;
